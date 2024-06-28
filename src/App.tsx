c579430b99cda72f0b7018d534f4aca0227a7311
SeroTomas <serocontacto@gmail.com>
2024-06-28 12:21:06 -0300

    layout and css

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import style from'./app.module.css';
+import style from './app.module.css';
+import Text from './components/Text';
 
 function App() {
   return (
     <main className={style.container}>
-
+      <section className={style.cardsSection}>
+        <Text />
+      </section>
     </main>
   )
 }
---
+++ src/components/Text.tsx
@@ -0,0 +1,12 @@
+import style from "../styles/text.module.css";
+
+const Text = () => {
+    return (
+        <div className={style.textContainer}>
+            <h2>Reliable, efficient delivery</h2>
+            <h2>Powered by Technology</h2>
+            <p>Our Artificial intelligence powered tools use millions of project data points to ensure that your project is successful</p>
+        </div>
+    )
+}
+export default Text
